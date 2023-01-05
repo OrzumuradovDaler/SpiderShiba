6ea2507ce9ab8ebb19410e5c5da284c5ec01109d
OrzumuradovDaler <118927695+OrzumuradovDaler@users.noreply.github.com>
2023-01-05 15:04:02 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,213 +1,83 @@
-let arr = [
-    {
-        name: "Shakhboz",
-        age: 18,
-        gender: "m"
-    },
-    {
-        name: "Alexandr",
-        age: 32,
-        gender: "m"
-    },
-    {
-        name: "Daler",
-        age: 15,
-        gender: "m"
-    },
-    {
-        name: "Muzaffar",
-        age: 19,
-        gender: "m"
-    },
-    {
-        name: "Eldor",
-        age: 16,
-        gender: "m"
-    },
-    {
-        name: "Ibragim",
-        age: 14,
-        gender: "w"
-    },
-    {
-        name: "Ravshan",
-        age: 18,
-        gender: "m"
-    },
-    {
-        name: "Timur",
-        age: 19,
-        gender: "w"
-    },
-    {
-        name: "Aziz",
-        age: 15,
-        gender: "w"
-    },
-    {
-        name: "Darya",
-        age: 26,
-        gender: "w"
-    },
-    {
-        name: "Mukhlisa",
-        age: 16,
-        gender: "w"
+    let arr = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut au`tem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+  
+            "completed":  true
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 6,
+            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 7,
+            "title": "illo expedita consequatur quia in",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 8,
+            "title": "quo adipisci enim quam ut ab",
+            "completed": true
+        },
+        {
+            "userId": 1,
+   
+            "id": 9,
+            "title": "molestiae perspiciatis ipsa",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 10,
+            "title": "illo est ratione doloremque quia maiores aut",
+            "completed": true
+        },
+    ]
+    let a = {
+        count: 0,
+        arrr: []
     }
-]
-let names = prompt('any name')
-if(names == arr[0].name){
-    console.log(
-        arr[0]
-    )
-
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(0,1)
-            console.log(
-                arr
-            )
-        }
-}else if(names == arr[1].name){
-    console.log(
-        arr[1]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(1,1)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[2].name){
-    console.log(
-        arr[2]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(2,2)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[3].name){
-    console.log(
-        arr[3]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(3,3)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[4].name){
-    console.log(
-        arr[4]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(4,4)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[5].name){
-    console.log(
-        arr[5]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(5,5)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[6].name){
-    console.log(
-        arr[6]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(6,6)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[7].name){
-    console.log(
-        arr[7]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(7,7)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[8].name){
-    console.log(
-        arr[8]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(8,8)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[9].name){
-    console.log(
-        arr[9]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(9,9)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else if(names == arr[10].name){
-    console.log(
-        arr[10]
-    )
-        let ask = confirm("Вы точно хотите удалить этого пользователя")
-        if(ask == true){
-            arr.splice(10,10)
-            console.log(
-                arr
-            )
-        } else{
-            console.log(arr)
-        }
-}
-else{
-    console.log("Пользователь не найден")
-}
+   
+    let b = {
+        count: 0,
+        arr: []
+    }
+let total = arr.filter(item => {
+   if(item.completed == false){
+  a.arrr.push(item.id)
+    console.log(`${a.arrr} false`)
+   }
+   else{
+b.arr.push(item.id)
+console.log(`${b.arr} true`)
+   }
+})
