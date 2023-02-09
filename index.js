7b1a935cb005817b2fc3444165e4ecc9034dfc54
OrzumuradovDaler <118927695+OrzumuradovDaler@users.noreply.github.com>
2023-02-09 15:28:15 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
-let plus = document.querySelector('.plus')
-let minus = document.querySelector('.minus')
-let counter = document.querySelector('.counter')
-let rnd = document.querySelector('.rnd')
-let reset = document.querySelector('.reset')
-counter.innerHTML = 0 
+let black = document.querySelector('.color1')
+let purple = document.querySelector('.color4')
+let gold = document.querySelector('.color2')
+let silver = document.querySelector('.color3')
+let stickypanel = document.querySelector('.stickypanel')
+let btnfirst = document.querySelector('.btnfirst')
+let btnsecond = document.querySelector('.btnsecond')
 
-plus.onclick = () => {
-    counter.innerHTML++ 
+black.onclick = () => {
+    stickypanel.classList.remove('goldfirst')
+    stickypanel.classList.remove('silverfirst')
+    stickypanel.classList.remove('deeppurple')
+    stickypanel.classList.toggle('spaceblackfirst')
+    btnsecond.classList.add('btnsecondobject')
 }
-minus.onclick = () => {
-    counter.innerHTML--
+gold.onclick = () => {
+    stickypanel.classList.remove('spaceblackfirst')
+    stickypanel.classList.remove('silverfirst')
+    stickypanel.classList.remove('deeppurple')
+    stickypanel.classList.toggle('goldfirst')
+    btnsecond.classList.add('btnsecondobject')
 }
-rnd.onclick = () => {
-    counter.innerHTML =  Math.floor(Math.random() * 100 )
+silver.onclick = () => {
+    stickypanel.classList.remove('spaceblackfirst')
+    stickypanel.classList.remove('deeppurple')
+    stickypanel.classList.remove('goldfirst')
+    stickypanel.classList.toggle('silverfirst')
+    btnsecond.classList.add('btnsecondobject')
 }
-reset.onclick = () => {
-    counter.innerHTML =  0
+purple.onclick = () => {
+    stickypanel.classList.remove('goldfirst')
+    stickypanel.classList.remove('spaceblackfirst')
+    stickypanel.classList.remove('silverfirst')
+    stickypanel.classList.toggle('deeppurple')
+    btnsecond.classList.add('btnsecondobject')
 }
+btnsecond.onclick = () => {
+    if(stickypanel.classList.contains('spaceblackfirst')){
+        stickypanel.classList.remove('goldsecond')
+        stickypanel.classList.remove('silversecond')
+        stickypanel.classList.remove('deeppurplesecond')
+        stickypanel.classList.toggle('spaceblacksecond') 
+}
+if(stickypanel.classList.contains('goldfirst')){
+    stickypanel.classList.remove('spaceblacksecond')
+    stickypanel.classList.remove('silversecond')
+    stickypanel.classList.remove('deeppurplesecond')
+    stickypanel.classList.toggle('goldsecond') 
+}
+if(stickypanel.classList.contains('silverfirst')){
+    stickypanel.classList.remove('spaceblacksecond')
+    stickypanel.classList.remove('deeppurplesecond')
+    stickypanel.classList.remove('goldsecond')
+    stickypanel.classList.toggle('silversecond') 
+}
+if(stickypanel.classList.contains('deeppurple')){
+    stickypanel.classList.remove('silversecond')
+    stickypanel.classList.remove('spaceblacksecond')
+    stickypanel.classList.remove('goldsecond')
+    stickypanel.classList.toggle('deeppurplesecond') 
+}
+}
+let modal = document.querySelector('.modal')
+let silka = document.querySelector('.silka')
+
+silka.onclick = () => {
+    modal.classList.toggle('modalokno')
+}
+console.log(silka);
+
