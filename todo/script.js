703ff9ced0613241fd0afcbb16bbf0dab4fa6316
OrzumuradovDaler <118927695+OrzumuradovDaler@users.noreply.github.com>
2023-03-12 11:17:03 +0500

    Add files via upload

---
+++ todo/script.js
@@ -0,0 +1,75 @@
+let cont = document.querySelector('.container')
+let form  = document.forms.creater
+let todos = [
+    {
+        id: 1,
+        completed: true,
+        time: "11:22",
+        task: 'Полететь в Дубай'
+    },
+    {
+        id: 2,
+        completed: false,
+        time: "11:46",
+        task: 'Полететь на работу'
+    }
+]
+
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+
+    let time = new Date().getHours() + ":" + new Date().getMinutes()
+    let todo = {
+        id: Math.random(),
+        completed: false,
+        time: time
+    }
+
+    let fm = new FormData(form)
+
+    fm.forEach((value, key) => {
+        todo[key] = value
+    })
+
+    todos.push(todo);
+    reload(todos) 
+}
+
+
+function reload(arr) {
+    cont.innerHTML = ""
+
+    for(let item of arr) {
+        let todoBox = document.createElement('div')
+        let left = document.createElement('div')
+        let right = document.createElement('div')
+        let h2 = document.createElement('h2')
+        let spanTime = document.createElement('span')
+        let cancelBtn = document.createElement('button')
+        let cancelImg = document.createElement('img')
+
+        todoBox.classList.add('box')
+        left.classList.add('left')
+        cancelImg.classList.add('cancelImg')
+        right.classList.add('right')
+
+        h2.innerHTML = item.task
+        spanTime.innerHTML = item.time
+
+        todoBox.append(left, right)
+        left.append(h2, spanTime)
+        right.append(cancelBtn)
+        cancelBtn.append(cancelImg)
+        cont.append(todoBox)
+
+        cancelBtn.onclick = () => {
+            todoBox.remove()
+        }
+        h2.onclick = () => {
+            h2.classList.toggle('cancel')
+        }        
+
+    }
+}
+reload(todos) 
